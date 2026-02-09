1934c5a9b5e9a642594019e8ef4cab206236523b
Memespls <105407159+Memespls@users.noreply.github.com>
2026-02-08 23:33:26 -0700

    changeptUpdate script.js

--- script.js
+++ script.js
@@ -150,7 +150,13 @@ loveMeter.addEventListener('input', () => {
         const extraWidth = overflowPercentage * window.innerWidth * 0.8;
         loveMeter.style.width = `calc(100% + ${extraWidth}px)`;
         loveMeter.style.transition = 'width 0.3s';
-        
+
+        if (value === 0) {
+        extraLove.classList.remove('hidden', 'super-love');
+        extraLove.textContent = config.loveMessages.sad;
+        loveMeter.style.width = '100%';
+        return;
+    }
         // Show different messages based on the value
         if (value >= 5000) {
             extraLove.classList.add('super-love');
@@ -161,10 +167,10 @@ loveMeter.addEventListener('input', () => {
         } else if (value > 100){
             extraLove.classList.remove('super-love');
             extraLove.textContent = config.loveMessages.normal;
-        } else if (value === 0){
-            extraLove.classList.remove('super-love');
-            extraLove.textContent = config.loveMessages.sad;
-        }
+        }// else if (value === 0){
+            //extraLove.classList.remove('super-love');
+            //extraLove.textContent = config.loveMessages.sad;
+       // }
     } else {
         extraLove.classList.add('hidden');
         extraLove.classList.remove('super-love');
